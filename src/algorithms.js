3433d45f8ef75ac31380dbde7554a9f5119aead9
Andrey Mudri <andreybeckert@gmail.com>
2026-03-25 14:36:49 -0300

    feat: adicionado algoritmos.js

---
+++ src/algorithms.js
@@ -0,0 +1,12 @@
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function fatoracao(n) {
+  if (n < 0) throw new Error("Número deve ser positivo");
+  if (n === 0 || n === 1) return 1;
+  return n * fatoracao(n - 1);
+}
+
+export default { fibonacci, fatoracao };
